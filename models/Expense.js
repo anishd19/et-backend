8b1a27e34f3dbfd88a3c268b82aaefd2d6276f2b
Anish Dhamodaran <anishd19@gmail.com>
2018-05-01 15:49:01 +0530

    finishes phase 1 of backend api

--- models/Expense.js
+++ models/Expense.js
@@ -1,20 +1,29 @@
 var mongoose = require('mongoose');
 
 var ExpenseSchema = new mongoose.Schema({
-  username: {type: String, unique: true},
-  categorylist: [{
-    name: String,
-    color: String
-  }],
-  expenses: [{
-    time: {type: Date, default: Date.now},
-    title: String,
-    amount: Number,
-    categorysel: [{
+  username: { type: String, unique: true },
+  categorylist: [new mongoose.Schema(
+    {
       name: String,
       color: String
-    }]
-  }]
+    },
+    { _id: false })
+  ],
+  expenses: [new mongoose.Schema(
+    {
+      time: Number,
+      title: String,
+      amount: Number,
+      categorysel: [new mongoose.Schema(
+        {
+        name: String,
+        color: String
+        },
+        { _id: false })
+      ]
+    },
+    { _id: false })
+  ]
 });
 
 mongoose.model('Expense', ExpenseSchema);
--- routes/api/expenses.js
+++ routes/api/expenses.js
@@ -3,23 +3,29 @@ var mongoose = require('mongoose');
 var Expense = mongoose.model('Expense');
 var auth = require('../auth');
 
-router.get('/', auth.required, function(req, res) {
+var initialExpense = {
+  "username": "",
+  "categorylist": [],
+  "expenses": []
+}
+
+router.get('/', auth.required, (req, res) => {
   var limit = 20;
   var offset = 0;
   var username = req.payload.username;
 
-  if(typeof req.query.limit !== 'undefined'){
+  if (typeof req.query.limit !== 'undefined'){
     limit = req.query.limit;
   }
 
-  if(typeof req.query.offset !== 'undefined'){
+  if (typeof req.query.offset !== 'undefined'){
     offset = req.query.offset;
   }
 
   Expense.findOne({username}, {expenses: {$slice: [+offset, +limit]}})
   // .slice('expenses', [offset, limit])
   .exec((err, data) => {
-    if(err) {
+    if (err) {
       console.log(`err: ${err}`);
       res.send(err);
     } else {
@@ -28,5 +34,68 @@ router.get('/', auth.required, function(req, res) {
     }
   });
 });
+
+router.post('/', auth.required, (req, res) => {
+  var username = req.body.username;
+  if(username !== req.payload.username) {
+    res.send('invalid username');
+  }
+  var tempExpense = {...initialExpense};
+  tempExpense.username = username;
+  // tempExpense.expenses = req.payload.expenses;
+  // tempExpense.categorylist = tempExpense.expenses.reduce(
+  //   (category, expense) => {
+  //     return (category.concat(...expense.categorysel));
+  //   }
+  // )
+  Expense.findOne({username})
+  .exec((err, data) => {
+    Promise.all([
+      !data ? 
+      Expense.create(tempExpense, (err, data) => {
+        if (err) {
+          res.send(err);
+        } else {
+          console.log(`data: ${data}`);
+        }
+      }) : null
+    ]).then((results) => {
+      Expense.findOneAndUpdate(
+        {username},
+        {
+          $addToSet: {
+            expenses: {$each: req.body.expenses},
+            categorylist: {$each: req.body.expenses[0].categorysel}
+          }
+        }
+      ).exec((err, data) => {
+        if(err) {
+          res.send(err);
+        } else {
+          console.log(`data: ${data}`);
+          res.json(data);
+        }
+      })
+    })
+  })
+});
+
+router.delete('/', auth.required, (req, res) => {
+  var username = req.body.username;
+  if(username !== req.payload.username) {
+    res.send('invalid username');
+  }
+  Expense.findOneAndUpdate(
+    {username},
+    {$pull: {'expenses': {time: req.body.time}}}
+  ).exec((err, data) => {
+    if(err) {
+      res.send(err);
+    } else {
+      console.log(`data: ${data}`);
+      res.json(data);
+    }
+  })
+})
 
 module.exports = router;
